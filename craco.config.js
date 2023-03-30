2b0abd2c6334a3fc5fd782bdddc903f859d7b78a
cailudys <cailudys@gmail.com>
2023-03-30 17:02:12 +0800

    项目引入代码规范工具

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 
-const resolve = (dir)=> path.resolve(__dirname, dir)
+const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
-    // 使用craco 实现修改使用create-react-app框架中的默认webpack配置。
-    webpack: {
-        alias: {
-            '@': resolve('src')
-        }
+  // 使用craco 实现修改使用create-react-app框架中的默认webpack配置。
+  webpack: {
+    alias: {
+      '@': resolve('src')
     }
+  }
 }
