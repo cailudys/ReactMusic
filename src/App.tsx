ab9135b5e711a9a875a0399483408ca636726594
cailudys <cailudys@gmail.com>
2023-03-31 18:45:08 +0800

    提交代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React, { Suspense } from 'react'
 import { useRoutes, Link } from 'react-router-dom'
+import { useSelector, shallowEqual } from 'react-redux'
+
 import routes from './router'
 
 function App() {
+  const { count, message } = useSelector(
+    (state: any) => ({
+      count: state.counter.count,
+      message: state.counter.message
+    }),
+    shallowEqual
+  )
+
   return (
     <div className="App">
       <div className="nav">
@@ -11,6 +21,8 @@ function App() {
         <Link to="/focus">关注</Link>
         <Link to="/download">下载客户端</Link>
       </div>
+      <h2>当前计数：{count}</h2>
+      <h2>当前消息：{message}</h2>
       <Suspense fallback="">
         <div className="main"> {useRoutes(routes)}</div>
       </Suspense>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
+// 第三方的东西
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import 'normalize.css'
+import { Provider } from 'react-redux'
 
+// 重置的东西
 import '@/assets/css/index.less'
 
+// 自己写的东西
+import store from './store'
 import App from '@/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 )
