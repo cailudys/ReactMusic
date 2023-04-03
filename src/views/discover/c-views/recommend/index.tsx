57946009e672cff23fb4a56b1da1f92b3073ec7e
cailudys <cailudys@gmail.com>
2023-04-03 18:55:17 +0800

    提交代码

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,11 +1,23 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import hyRequest from '@/service'
 
 interface Iprops {
   children?: ReactNode
 }
 
 const Recommend: FC<Iprops> = () => {
+  const [banners, setBanners] = useState<any[]>([])
+
+  // 测试网络请求
+  useEffect(() => {
+    hyRequest
+      .get({
+        url: '/banner'
+      })
+      .then((res) => console.log(res))
+  }, [])
+
   return <>Recommend</>
 }
 
