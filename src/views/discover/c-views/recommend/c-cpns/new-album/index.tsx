5c6b818b728209a2cc17e54d5c7aca3b8eca033a
cailudys <cailudys@gmail.com>
2023-04-07 17:30:37 +0800

    发现添加轮播图之后，导致整个页面的样式发生了变化，这时问什么？

--- src/views/discover/c-views/recommend/c-cpns/new-album/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/new-album/index.tsx
@@ -1,5 +1,7 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
+import { Carousel } from 'antd'
+
 import { NewAlbumWapper } from './style'
 import AreaHeaderV1 from '@/components/area-header-v1'
 
@@ -13,6 +15,14 @@ const NewAlbum: FC<Iprops> = () => {
       <AreaHeaderV1 title="新碟上架" moreLink="/disciver/album"></AreaHeaderV1>
       <div className="content">
         <button className="sprite_02 arrow arrow-left"></button>
+        {/* 发现添加轮播图之后，导致整个页面的样式发生了变化，这时问什么？ */}
+        {/* <div className="banner">
+          <Carousel>
+            {[1, 2].map((item) => {
+              return <h1 key={item}>{item}</h1>
+            })}
+          </Carousel>
+        </div> */}
         <button className="sprite_02 arrow arrow-right"></button>
       </div>
     </NewAlbumWapper>
