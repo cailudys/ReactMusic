e28a260b393ebc7279a90a403a2a042110437432
cailudys <cailudys@gmail.com>
2023-04-12 13:28:55 +0800

    完成播放和暂停播放的功能

--- src/views/player/app-player-bar/index.tsx
+++ src/views/player/app-player-bar/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useRef, useState } from 'react'
 import type { FC, ReactNode } from 'react'
 import { Slider } from 'antd'
 import { Link } from 'react-router-dom'
@@ -9,29 +9,70 @@ import {
   BarPlayerInfo,
   BarOperator
 } from './style'
-import { useAppSelector } from '@/store'
+
+import { useAppDispatch, useAppSelector } from '@/store'
 import { getImageSize } from '@/utils/format'
+import { getSongPlayUrl } from '@/utils/handle-player'
 
 interface Iprops {
   children?: ReactNode
 }
 
 const AppPlayerBar: FC<Iprops> = () => {
+  /** 组件内部定义的数据 */
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [progress, setProgress] = useState(0)
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  /** 从redux中获取数据 */
   const { currentSong } = useAppSelector((state) => ({
     currentSong: state.player.currentSong
   }))
 
+  // const dispatch = useAppDispatch()
+
+  /** 组件内的副作用操作 */
+  useEffect(() => {
+    // 1.播放音乐
+    audioRef.current!.src = getSongPlayUrl(33894312)
+    audioRef.current
+      ?.play()
+      .then(() => {
+        setIsPlaying(true)
+        console.log('歌曲播放成功')
+      })
+      .catch((err) => {
+        setIsPlaying(false)
+        console.log('歌曲播放失败:', err)
+      })
+
+    // // 2.获取音乐的总时长
+    // setDuration(currentSong.dt)
+  }, [currentSong])
+
+  /** 组件内部的事件处理 */
+  function handlePlayBtnClick() {
+    console.log('33333333333', isPlaying)
+    // 1.控制播放器的播放/暂停
+    isPlaying
+      ? audioRef.current?.pause()
+      : audioRef.current?.play().catch(() => setIsPlaying(false))
+
+    // 2.改变isPlaying的状态
+    setIsPlaying(!isPlaying)
+  }
+
   return (
     <PlayerBarWrapper className="sprite_playbar">
       <div className="content wrap-v2">
-        <BarControl>
+        <BarControl isPlaying={isPlaying}>
           <button
             className="btn sprite_playbar prev"
             onClick={() => null}
           ></button>
           <button
             className="btn sprite_playbar play"
-            // onClick={() => null}
+            onClick={handlePlayBtnClick}
           ></button>
           <button
             className="btn sprite_playbar next"
@@ -83,6 +124,11 @@ const AppPlayerBar: FC<Iprops> = () => {
           </div>
         </BarOperator>
       </div>
+      <audio
+        ref={audioRef}
+        // onTimeUpdate={handleTimeUpdate}
+        // onEnded={handleTimeEnded}
+      />
     </PlayerBarWrapper>
   )
 }
